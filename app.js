3e897b83b69fe0fb19e75f1bbe31fcbf205cf269
Teddy <edmondteddy@hotmail.fr>
2022-06-09 10:21:34 +0200

    finished
    desktop done, mobile done
    the animation will be updated in the futur.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Set the date we're counting down to
-const countDownDate = new Date('Jun 17, 2022 17:11:00').getTime()
-
+const countDownDate = new Date('Jun 18, 2022 17:11:00').getTime()
+//Select our target in the html
 const daysHtml = document.getElementById('days_count')
 const hoursHtml = document.getElementById('hours_count')
 const minutesHtml = document.getElementById('minutes_count')
 const secondsHtml = document.getElementById('seconds_count')
-
+//the big function thing basically some maths betwin the furtur and now
 const sec = setInterval(function () {
   const now = new Date().getTime()
 
@@ -16,27 +16,10 @@ const sec = setInterval(function () {
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
   const seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-  //   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  //   + minutes + "m " + seconds + "s ";
-  daysHtml.innerHTML = days
-  hoursHtml.innerHTML = hours
+  //send it to the html
+  daysHtml.innerHTML = days < 10 ? '0' + days : days
+  hoursHtml.innerHTML = hours < 10 ? '0' + hours : hours
   minutesHtml.innerHTML = minutes
   secondsHtml.innerHTML = seconds
-
-  const secondsClass = document.getElementById('seconds')
-  const minutesClass = document.getElementById('minutes')
-
-  //animate second every second
-  if (secondsHtml.innerHTML == secondsHtml.innerHTML) {
-    secondsClass.classList.add('animate')
-    secondsHtml.classList.add('animate_text')
-  }
-  //animate minutes
-  if (secondsHtml.innerHTML == 0) {
-    minutesClass.classList.add('animate')
-    minutesHtml.classList.add('animate_text')
-  } else {
-    minutesClass.classList.remove('animate')
-    minutesHtml.classList.remove('animate_text')
-  }
+  //afterwards you can have a function to stop counting when it reaches 0
 }, 1000)
